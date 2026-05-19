e8c35df8bc3d74abbaab4231f13d74e1cf13273b
付向明 <fuxiangming@hanokl.com>
2026-05-19 17:36:31 +0800

    init

--- test_rtf.rs
+++
@@ -1,8 +0,0 @@
-use std::path::Path;
-use coi::parser::DocumentParser;
-
-fn main() {
-    let parser = DocumentParser::new();
-    let result = parser.parse(Path::new("test-data/knowledge_base/blockchain.rtf")).unwrap();
-    println!("Parsed content:\n{}", result.content);
-}
